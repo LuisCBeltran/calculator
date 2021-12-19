51db5f356b78b025527050c399daea63d343a5c9
Luis C. Beltran <lcarlosbeltran@gmail.com>
2021-12-19 15:29:01 -0500

    almost finished, percentage to be done

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let toDisplay = []
 let operand1
 let operand2
-let isOperatorOn = false
-let operator
+let operator = 0
+let result 
 
+// Operators: no operator = 0, + = 1, - = 2, * = 3, / = 4, 
 
-let pushNumber = function (number) {
+// Write numbers on display
+const pushNumber = function (number) {
 
-    if (!isOperatorOn) {
+    if (operator === 0 && result === undefined) {
 
         toDisplay.push(number)
         let stringToDisplay = toDisplay.join('')
@@ -16,11 +18,14 @@ let pushNumber = function (number) {
         document.getElementById('displayNums').innerHTML = stringToDisplay
 
         // Assign value to operand 1
-        operand1 = parseInt(stringToDisplay)
+        operand1 = parseFloat(stringToDisplay)
+
+        operand2 = 0
 
         console.log(operand1, operand2)
+        console.log(toDisplay)
 
-    } else {
+    } else if (operator > 0 && operator < 5) {
 
         toDisplay.push(number)
         let stringToDisplay = toDisplay.join('')
@@ -29,39 +34,201 @@ let pushNumber = function (number) {
         document.getElementById('displayNums').innerHTML = stringToDisplay
 
         // Assign value to operand 1
-        operand2 = parseInt(stringToDisplay)
+        operand2 = parseFloat(stringToDisplay)
+
+        result = undefined
 
         console.log(operand1, operand2)
     }
 }
 
-let operation = function (num) {
 
-    isOperatorOn = true
+const operation = function (num) {
 
+    // Reset display
     toDisplay = []
+    console.log(operator)
 
-    console.log(operand2, operand1)
+    if (operand1 === undefined && operand2 === undefined) {
 
-    if (operand1 === undefined || operand2 === undefined) {
+        operator = 0
 
+    } else if (operator !== 0 && result === undefined) {
 
-    } else if (num === 1) {
+        savedOperation(operator)
+        operator = num
 
-        // Addition
-        let result = operand1 + operand2
-        operand1 = result
-        let stringResult = result.toString()
-        document.getElementById('displayNums').innerHTML = stringResult
+    } else if (operator !== 0 && result !== undefined) {
+        
+        operator = num
+ 
+    } else if (operator === 0) {
 
-    } else if (num === 2) {
+        operator = num
 
-        //Substraction
-        let result = operand1 - operand2
-        operand1 = result
-        console.log(operand1, operand2, result)
-        let stringResult = result.toString()
-        document.getElementById('displayNums').innerHTML = stringResult
     }
 
+    console.log(operator)
+    console.log(result)
+
+}
+
+// Operations
+let savedOperation = function (operator) {
+    
+    // Addition
+    if (operator === 1) {
+
+        result = operand1 + operand2
+        let resultFixed = result.toFixed(5)
+        let stringResult = resultFixed.toString()
+
+        document.getElementById('displayNums').innerHTML = parseFloat(stringResult)
+
+        operand1 = result
+
+    // Substraction
+    } else if (operator === 2) {
+
+        result = operand1 - operand2
+        let resultFixed = result.toFixed(5)
+        let stringResult = resultFixed.toString()
+
+        document.getElementById('displayNums').innerHTML = parseFloat(stringResult)
+
+        operand1 = result
+    
+    // Multiplication
+    } else if (operator === 3) {
+
+        result = operand1 * operand2
+        let resultFixed = result.toFixed(5)
+        let stringResult = resultFixed.toString()
+
+        document.getElementById('displayNums').innerHTML = parseFloat(stringResult)
+
+        operand1 = result
+    
+    // Division
+    } else if (operator === 4) {
+
+        result = operand1 / operand2
+        let resultFixed = result.toFixed(5)
+        let stringResult = resultFixed.toString()
+
+        document.getElementById('displayNums').innerHTML = parseFloat(stringResult)
+
+        operand1 = result
+    }
+}
+
+// Equals
+
+const equals = function () {
+    if (operator > 0 && operator < 5 && result === undefined) {
+        savedOperation(operator)
+        operator = 0
+    }
+}
+
+// Change to postive or negative
+const changeSign = function () {
+
+    if (operator === 0 && result === undefined) {
+        operand1 = operand1 * -1
+        document.getElementById('displayNums').innerHTML = parseFloat(operand1)
+
+    } else if (operator > 0 && operator < 5 && result === undefined) {
+        operand2 = operand2 * -1
+        document.getElementById('displayNums').innerHTML = parseFloat(operand2)
+    }
+}
+
+// Delete
+const deleteNum = function () {
+
+    if (operator === 0 && result === undefined && toDisplay.length > 0) {
+        toDisplay.pop()
+        let stringToDisplay = toDisplay.join('')
+
+        // Render strings into display
+        if (toDisplay.length > 0) {
+            document.getElementById('displayNums').innerHTML = stringToDisplay
+
+            // Assign value to operand 1
+            operand1 = parseFloat(stringToDisplay)
+
+            operand2 = 0
+
+        } else {
+
+            document.getElementById('displayNums').innerHTML = 0
+            operand1 = 0
+            operand2 = 0
+        }
+
+        console.log(operand1, operand2)
+        console.log(toDisplay)
+
+    } else if (operator > 0 && operator < 5 && result === undefined && toDisplay.length > 0) {
+        
+        toDisplay.pop()
+        let stringToDisplay = toDisplay.join('')
+
+        // Render strings into display
+        if (toDisplay.length > 0) {
+            document.getElementById('displayNums').innerHTML = stringToDisplay
+
+            // Assign value to operand 1
+            operand2 = parseFloat(stringToDisplay)
+    
+            result = undefined
+
+        } else {
+
+            document.getElementById('displayNums').innerHTML = 0
+
+            // Assign value to operand 1
+            operand2 = 0
+    
+            result = undefined
+        }
+
+        console.log(operand1, operand2)
+    }
+}
+
+// Reset AC
+const reset = function () {
+    toDisplay = []
+    operand1 = undefined
+    operand2 = undefined
+    operator = 0
+    result = undefined
+    document.getElementById('displayNums').innerHTML = 0
+}
+
+//  Power
+const power = function (exp) {
+    if (operator === 0 && result === undefined && toDisplay.length > 0) {
+        operand1 = Math.pow(operand1, exp)
+        document.getElementById('displayNums').innerHTML = operand1.toFixed(5)
+
+    } else if (operator !== 0 && result !== undefined) {
+        result = Math.pow(result, exp)
+        document.getElementById('displayNums').innerHTML = result.toFixed(5)
+    }
+}
+
+// Square root
+
+const square = function () {
+    if (operator === 0 && result === undefined && toDisplay.length > 0) {
+        operand1 = Math.sqrt(operand1)
+        document.getElementById('displayNums').innerHTML = operand1.toFixed(5)
+
+    } else if (operator !== 0 && result !== undefined) {
+        result = Math.sqrt(result)
+        document.getElementById('displayNums').innerHTML = result.toFixed(5)
+    }
 }
